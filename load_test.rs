0f1bd2f49144b4306ededa85c24afbf2e9419fe9
Zach Leong <earthospreyhd@gmail.com>
2018-12-31 17:16:55 +1100

    added a little

--- load_test.rs
+++ load_test.rs
@@ -1,3 +1,12 @@
+use std::thread;
+const thread_num: i32 = 10;
+
 fn main () {
+    let handle: [thread; thread_num];
+    for i in 0..10 {
+        let handle[i] = thread::spawn(|| {
+            
+        });
+    }
 
 }
